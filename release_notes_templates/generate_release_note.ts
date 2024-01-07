435aafbd4c23971b1606307d24d50c7562481d4a
qiwen li <qiwen_li@brown.edu>
2024-01-06 20:54:22 -0800

    Revert "add set-up instructions and an example for the release-note script"
    This reverts commit 328a41cfca4f8d0065165c126e7382293b1db023.
    
    Signed-off-by: qiwen li <qiwen_li@brown.edu>
    
    revert

--- release_notes_templates/generate_release_note.ts
+++ release_notes_templates/generate_release_note.ts
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { resolve } from "path";
-import { readFileSync, writeFileSync, Dirent, renameSync, rm } from "fs";
-import { load as loadYaml } from "js-yaml";
-import { mkdir, readdir } from "fs/promises";
-import { version as pkgVersion } from "../../package.json";
+import { resolve } from 'path';
+import { readFileSync, writeFileSync, Dirent, renameSync, rm } from 'fs';
+import { load as loadYaml } from 'js-yaml';
+import { mkdir, readdir } from 'fs/promises';
+import { version as pkgVersion } from '../../package.json';
 import {
   validateFragment,
   getCurrentDateFormatted,
@@ -18,21 +18,21 @@ import {
   SectionKey,
   releaseNotesDirPath,
   filePath,
-} from "./generate_release_note_helper";
+} from './generate_release_note_helper';
 
-// Function to add content after the 'Unreleased' section in the CHANGELOG.md
+// Function to add content after the 'Unreleased' section in the changelog
 function addContentAfterUnreleased(path: string, newContent: string): void {
-  let fileContent = readFileSync(path, "utf8");
-  const targetString = "## [Unreleased]";
+  let fileContent = readFileSync(path, 'utf8');
+  const targetString = '## [Unreleased]';
   const targetIndex = fileContent.indexOf(targetString);
 
   if (targetIndex !== -1) {
-    const endOfLineIndex = fileContent.indexOf("\n", targetIndex);
+    const endOfLineIndex = fileContent.indexOf('\n', targetIndex);
 
     if (endOfLineIndex !== -1) {
       fileContent =
         fileContent.slice(0, endOfLineIndex + 1) +
-        "\n" +
+        '\n' +
         newContent +
         fileContent.slice(endOfLineIndex + 1);
     } else {
@@ -56,24 +56,21 @@ async function deleteFragments() {
 // Read fragment files and populate sections
 async function readFragments() {
   // Initialize sections
-  const sections: Changelog = Object.fromEntries(
+  const sections: Changelog = (Object.fromEntries(
     Object.keys(SECTION_MAPPING).map((key) => [key, []])
-  ) as unknown as Changelog;
+  ) as unknown) as Changelog;
 
   const fragmentPaths = await readdir(fragmentDirPath, { withFileTypes: true });
   for (const fragmentFilename of fragmentPaths) {
     // skip non yml or yaml files
-    if (
-      !fragmentFilename.name.endsWith(".yml") &&
-      !fragmentFilename.name.endsWith(".yaml")
-    ) {
+    if (!fragmentFilename.name.endsWith('.yml') && !fragmentFilename.name.endsWith('.yaml')) {
       // eslint-disable-next-line no-console
       console.warn(`Skipping non yml or yaml file ${fragmentFilename.name}`);
       continue;
     }
 
     const fragmentPath = resolve(fragmentDirPath, fragmentFilename.name);
-    const fragmentContents = readFileSync(fragmentPath, { encoding: "utf-8" });
+    const fragmentContents = readFileSync(fragmentPath, { encoding: 'utf-8' });
 
     validateFragment(fragmentContents);
 
@@ -93,31 +90,24 @@ async function moveFragments(fragmentPaths: Dirent[]): Promise<void> {
   // Move fragment files to temp fragments folder
   for (const fragmentFilename of fragmentPaths) {
     const fragmentPath = resolve(fragmentDirPath, fragmentFilename.name);
-    const fragmentTempPath = resolve(
-      fragmentTempDirPath,
-      fragmentFilename.name
-    );
+    const fragmentTempPath = resolve(fragmentTempDirPath, fragmentFilename.name);
     renameSync(fragmentPath, fragmentTempPath);
   }
 }
 
 function generateChangelog(sections: Changelog) {
   // Generate changelog sections
-  const changelogSections = Object.entries(sections).map(
-    ([sectionKey, entries]) => {
-      const sectionName = SECTION_MAPPING[sectionKey as SectionKey];
-      return entries.length === 0
-        ? `### ${sectionName}`
-        : `### ${sectionName}\n\n${entries
-            .map((entry) => ` - ${entry}`)
-            .join("\n")}`;
-    }
-  );
+  const changelogSections = Object.entries(sections).map(([sectionKey, entries]) => {
+    const sectionName = SECTION_MAPPING[sectionKey as SectionKey];
+    return entries.length === 0
+      ? `### ${sectionName}`
+      : `### ${sectionName}\n\n${entries.map((entry) => ` - ${entry}`).join('\n')}`;
+  });
 
   // Generate full changelog
   const currentDate = getCurrentDateFormatted();
   const changelog = `## [${pkgVersion}-${currentDate}](
-  ${changelogSections.join("\n\n")}
+  ${changelogSections.join('\n\n')}
   `;
   // Update changelog file
   addContentAfterUnreleased(filePath, changelog);
@@ -128,9 +118,7 @@ function generateReleaseNote(changelogSections: string[]) {
   // Generate release note
   const releaseNoteFilename = `opensearch-dashboards.release-notes-${pkgVersion}.md`;
   const releaseNoteHeader = `# VERSION ${pkgVersion} Release Note`;
-  const releaseNote = `${releaseNoteHeader}\n\n${changelogSections.join(
-    "\n\n"
-  )}`;
+  const releaseNote = `${releaseNoteHeader}\n\n${changelogSections.join('\n\n')}`;
   writeFileSync(resolve(releaseNotesDirPath, releaseNoteFilename), releaseNote);
 }
 
